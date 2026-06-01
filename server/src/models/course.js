de8e62c7d7eab673b698d06b306663ebf7a841e4
Charumehra <Charumehracm2004@gmail.com>
2026-06-01 22:44:51 +0530

    Enhance LMS Schema

--- server/src/models/course.js
+++ server/src/models/course.js
@@ -1,8 +1,14 @@
 const mongoose = require("mongoose");
 
 const lectureSchema = new mongoose.Schema({
-  title: String,
-  videoUrl: String,
+  title: {
+    type: String,
+    required: true,
+  },
+  videoUrl: {
+    type: String,
+    required: true,
+  },
   publicId: String,
   duration: String,
 });
@@ -37,6 +43,7 @@ const courseSchema = new mongoose.Schema(
     instructor: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
+      required: true
     },
     lectures: [lectureSchema],
     enrolledStudents: [
