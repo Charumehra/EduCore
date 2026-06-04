01e0495d1128c80fa4301afb2c85c4bbece72af9
Charumehra <Charumehracm2004@gmail.com>
2026-06-04 23:11:29 +0530

    chore: update production API URL

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://prodesk-capstone-edu-core-eta.vercel.app"],
     credentials: true,
   }),
 );
