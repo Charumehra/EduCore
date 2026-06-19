b1986eeaaa804bf69bd07aba2e8ef70d8e427358
Charumehra <Charumehracm2004@gmail.com>
2026-06-19 15:28:58 +0530

    fix(routes): correct validate middleware import path

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -3,7 +3,7 @@ const {registerUser, logoutUser, getUserInfo, loginUser} = require("../controlle
 const { authMiddleware } = require("../middlewares/auth.middleware");
 const router = express.Router();
 const { registerSchema, loginSchema } = require("../validators/authValidator");
-const validate = require("../middlewares/validate.middleware");
+const validate = require("../middlewares/validate");
 
 router.post("/register", validate(registerSchema), registerUser);
 router.post("/login", validate(loginSchema), loginUser);
